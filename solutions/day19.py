7629df003a06a5b2b40ec04a203fcddab39549b1
Robert-Davie <robertjdavie2@gmail.com>
2024-12-19 11:53:08 +0000

    day 19

---
+++ solutions/day19.py
@@ -0,0 +1,65 @@
+from enum import Enum
+
+
+class Stripes(Enum):
+    WHITE = "w"
+    BLUE = "u"
+    BLACK = "b"
+    GREEN = "g"
+    RED = "r"
+
+
+colour_dict = {
+    Stripes.WHITE: 0,
+    Stripes.BLUE: 1,
+    Stripes.BLACK: 2,
+    Stripes.GREEN: 3,
+    Stripes.RED: 4,
+}
+
+
+checked_result = {}
+
+
+def replace_all(input_in):
+    copy = input_in
+    for colour in colour_dict.keys():
+        copy = copy.replace(colour.value, str(colour_dict[colour]))
+    return [int(i) for i in list(copy)]
+
+
+with open("../inputs/day19.txt") as f:
+    lines = [line.strip() for line in f.readlines()]
+
+components = [i.strip() for i in lines[0].split(",")]
+components = [replace_all(i) for i in components]
+to_make = lines[2:]
+to_make = [replace_all(i) for i in to_make]
+
+
+def can_be_made(list_in):
+    if tuple(list_in) in checked_result.keys():
+        return checked_result[tuple(list_in)]
+    if not list_in:
+        return True
+    for component in components:
+        if len(list_in) < len(component):
+            continue
+        if list_in[:len(component)] == component:
+            if can_be_made(list_in[len(component):]):
+                checked_result[tuple(list_in)] = True
+                return True
+    checked_result[tuple(list_in)] = False
+    return False
+
+
+counter = 0
+checked = 0
+for item in to_make:
+    if can_be_made(item):
+        counter += 1
+    checked += 1
+    print(checked)
+
+
+print(f"result = {counter}")
